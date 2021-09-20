72bd8899f8c591d634545649e9739710222ce52c
Mauro <tordycapz@gmail.com>
2021-09-20 11:08:04 -0300

    Cambios varios
    *Cambio de personaje
    *Cambio de Background
    *Cambio de posicionamiento
    *Agregado subir y bajar libremente
    *Arreglado doble texto de Nivel

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ WIN = pygame.display.set_mode(((WIDTH, HEIGHT)))
 pygame.display.set_caption("Un jueguito") #Seteo el titulo de la ventana
 
 #Cargo las imagenes del personaje
-TOM_1 = pygame.transform.scale(pygame.image.load(os.path.join("images/tom", "tom-1.png")), (120, 200))
+TOM_1 = pygame.transform.scale(pygame.image.load(os.path.join("images/tom", "tom-1.png")), (120, 120))
 TOM_2 = pygame.image.load(os.path.join("images/tom", "tom-2.png"))
 TOM_2 = pygame.image.load(os.path.join("images/tom", "tom-3.png"))
 
 #Cargo la imagen del fondo
-BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("images/background", "background.jpg")), (WIDTH, HEIGHT)) #Escalo la imagen de BG al tamaño de la pantalla
+BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("images/background", "background-2.png")), (WIDTH*2, HEIGHT)) #Escalo la imagen de BG al tamaño de la pantalla
 
 #Colores (RED, GREEN, BLUE) (RGB)
 FONT_RED = (252, 3, 69)
@@ -50,13 +50,13 @@ def main():
 
     player_vel = 5 #Velocidad max en la que se va a mover un personaje al presionar una tecla
 
-    character = Player(350, 350) #Posiciono el personaje X,Y
+    character = Player(350, 420) #Posiciono el personaje X,Y
 
     def redraw_window(): 
         WIN.blit(BACKGROUND, (0,0)) #Coloco el BG
-
+         
         #Escribo el texto de nivel y vida
-        lives_label = main_font.render(f"Nivel: {lives}", 1, FONT_RED)
+        lives_label = main_font.render(f"Vidas: {lives}", 1, FONT_RED)
         level_lebel = main_font.render(f"Nivel: {level}", 1, FONT_RED)
         WIN.blit(lives_label, (10 , 10)), WIN.blit(level_lebel, (WIDTH - lives_label.get_width() - 10, 10)) #Ancho de la pantalla - ancho de la palabra - 10
         
@@ -66,7 +66,6 @@ def main():
 
     while run:
         clock.tick(FPS)
-        redraw_window() #llamo a la funcion para updatear la pantalla
 
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT: #Cuando el usuario preciona el boton de cerrar (X)
@@ -77,5 +76,11 @@ def main():
             character.x -= player_vel 
         if keys[pygame.K_d] or keys[pygame.K_RIGHT] and character.x +  player_vel + 50 < WIDTH: #Derecha (Despues del and compruebo que el character no pase los bordes de las ventanas)
             character.x += player_vel
+        if keys[pygame.K_w] or keys[pygame.K_UP] and character.y +  player_vel + 50 > 0: #Arriba
+            character.y -= player_vel
+        if keys[pygame.K_s] or keys[pygame.K_DOWN] and character.y +  player_vel + 50 < (HEIGHT - 100): #Arriba
+            character.y += player_vel
+
+        redraw_window() #llamo a la funcion para updatear la pantalla
 
 main() #Llamo a la funcion Main par iniciar el juego
