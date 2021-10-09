3cdc22a81d89b4b509a17d2598ff1e03a0a27399
Mauro <tordycapz@gmail.com>
2021-10-09 20:14:31 -0300

    Cambio de codigo, nuevo juego
    Se cambio el codigo para updatear el nuevo modo de juego:
    *Se agregaron carpinchos
    *Las imagenes ahora de los carpinchos son de 190x190 perfectos
    *Se cambio el titulo del juego
    *Se cambio la manera que spawnean los carpinchos

--- main.py
+++ main.py
@@ -1,46 +1,31 @@
 import pygame
 import os, time, random
 
-from pygame import mixer #Mixer para el audio
+from pygame import mixer
+
 pygame.font.init() #Importo para pdoer usar fuentes
 
 #Voy a crear las propiedades de la ventana
 WIDTH, HEIGHT = 800, 600 #El tamaño de X e Y
 WIN = pygame.display.set_mode(((WIDTH, HEIGHT)))
-pygame.display.set_caption("Un jueguito") #Seteo el titulo de la ventana
+pygame.display.set_caption("Carpinchometro") #Seteo el titulo de la ventana
 
 #Cargo las imagenes del personaje
-TOM_1 = pygame.transform.scale(pygame.image.load(os.path.join("images/tom", "tom-1.png")), (120, 120))
-TOM_2 = pygame.image.load(os.path.join("images/tom", "tom-2.png"))
-TOM_3 = pygame.image.load(os.path.join("images/tom", "tom-3.png"))
+PLAYER = pygame.image.load(os.path.join("images/carpincho", "carpincho-1.png"))
 
-#Cargo imagenes del enemigo
-ENEMY_1 = pygame.transform.scale(pygame.image.load(os.path.join("images/enemy", "enemy-1.png")), (120, 120))
-ENEMY_2 = pygame.transform.scale(pygame.image.load(os.path.join("images/enemy", "enemy-2.png")), (120, 120))
-ENEMY_3 = pygame.transform.scale(pygame.image.load(os.path.join("images/enemy", "enemy-3.png")), (120, 120))
-ENEMY_4 = pygame.transform.scale(pygame.image.load(os.path.join("images/enemy", "enemy-4.png")), (120, 120))
+#Cargo las imagenes del carpincho corriendo (190px x 190px)
+CARPI_1 = pygame.image.load(os.path.join("images/carpincho", "carpincho-1.png"))
+CARPI_2 = pygame.image.load(os.path.join("images/carpincho", "carpincho-2.png"))
+CARPI_3 = pygame.image.load(os.path.join("images/carpincho", "carpincho-3.png"))
+CARPI_4 = pygame.image.load(os.path.join("images/carpincho", "carpincho-4.png"))
+CARPI_5 = pygame.image.load(os.path.join("images/carpincho", "carpincho-5.png"))
 
-#Cargo lasers o disparos
-LASER_BLUE = pygame.image.load(os.path.join("images/disparos", "pixel_laser_blue.png"))
-LASER_GREEN = pygame.image.load(os.path.join("images/disparos", "pixel_laser_green.png"))
-LASER_RED = pygame.image.load(os.path.join("images/disparos", "pixel_laser_red.png"))
-LASER_YELLOW = pygame.image.load(os.path.join("images/disparos", "pixel_laser_yellow.png"))
 
-#Cargo la imagen del fondo
-BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("images/background", "background-2.png")), (WIDTH*2, HEIGHT)) #Escalo la imagen de BG al tamaño de la pantalla
-
-#Colores (RED, GREEN, BLUE) (RGB)
-FONT_RED = (252, 3, 69) #Colores de la fuente
-
-#Cargo la musica
-pygame.init()
-pygame.mixer.music.set_volume(0.05) #Seteo el volumen
-backgroud_music = mixer.music.load("music/battle.wav")
-mixer.music.play(-1)
-
+#Cargo imagen del background
+BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("images/background", "back.png")), (WIDTH, HEIGHT)) #Escalo la imagen del BG para que siempre se adapten a la pantalla
 #Clase del personaje
 class Character:
-    def __init__(self, x, y, health=100):
+    def __init__(self, x, y, health = 100):
         self.x = x
         self.y = y
         self.health = health
@@ -59,90 +44,83 @@ class Character:
         return self.character_img.get_height()  #Obtengo el tamaño del personaje
 
 class Player(Character):
-    def __init__(self, x, y, health = 100): #Default vida maxima
-        super().__init__(x, y, health=health) #Inicializa todo lo que esta en Character
-        self.character_img = TOM_1
-        self.laser_img = LASER_RED #Le asigno un laser o disparo al jugador
-        self.mask = pygame.mask.from_surface(self.character_img) #Crea una mascara de pixel (Permite hacer colisiones)
-        self.max_healt = health #Lo utilizamos para saber cual es la vida maxima que tenga el jugador
+    def __init__(self, x, y, health=100): #Default vida maxima
+        super().__init__(x, y, health=health)
+        self.character_img = PLAYER
+        self.mask = pygame.mask.from_surface(self.character_img) #Crea mascara de colision sobre el personaje
+        self.max_healt = health #Para saber la vida maxima del jugador
 
-class Enemy(Character):
+class Enemy(Character): #Clase para los carpinchos
 
-    #Hago un mapa de colores de enemigos con sus disparos
-    COLOR_MAP = {
-                "blue": (ENEMY_1, LASER_BLUE),
-                "green": (ENEMY_2, LASER_GREEN),
-                "yellow": (ENEMY_3, LASER_YELLOW)
-                }
-
-    def __init__(self, x, y, color, health = 100):
+    def __init__(self, x, y, health=100):
         super().__init__(x, y, health)
-        self.character_img, self.laser_img = self.COLOR_MAP[color]
-        self.mask = pygame.mask.from_surface(self.character_img) #Crea una mascara de pixel (Permite hacer colisiones)
+        self.character_img = CARPI_1
+        self.mask = pygame.mask.from_surface(self.character_img) #Mascara del carpincho
 
-    def move(self, vel):    #El movimiento para enemigos
-        self.y += vel
+    def move(self, vel):
+            self.x -= vel
+
+#Colores fuentes
+FONT_RED = (252, 3, 69)
 
 def main():
-    run = True #Mantiene la ventana abierta mientras este en True
-    FPS = 60 #Los fps
-    level = 0
-    lives = 5
-    main_font = pygame.font.SysFont("comicsans", 50)
+    run = True  #Mantiene la ventana abierta mientras sea True
+    FPS = 60    #Seteo la velocidad de fotogramas
+    level = 0   #Nivel
+    lives = 5   #Vida
+    main_font = pygame.font.SysFont("comicsans", 50)    #Fuente
 
     enemies = []
-    enemy_vel = 1
-    wave_lenght = 5 #El tamaño para la ola de enemigos
+    enemy_vel = 5   #Velocidad del enemigo
+    wave_lenght = 0 #Tamaño de la ola
 
-    player_vel = 5 #Velocidad max en la que se va a mover un personaje al presionar una tecla
-
-    player = Player(350, 420) #Posiciono el personaje X,Y
-
+    player_vel = 5
+    player = Player( (WIDTH/2)-95, (HEIGHT - 190)) #Posiciono al personaje en X,Y (Si tiene 190px x 190x)
+    
     clock = pygame.time.Clock()
 
-    def redraw_window(): 
+    def redraw_window():
         WIN.blit(BACKGROUND, (0,0)) #Coloco el BG
-         
-        #Escribo el texto de nivel y vida
+
+        #Escribo el texto de la vida
         lives_label = main_font.render(f"Vidas: {lives}", 1, FONT_RED)
-        level_lebel = main_font.render(f"Nivel: {level}", 1, FONT_RED)
-        WIN.blit(lives_label, (10 , 10)), WIN.blit(level_lebel, (WIDTH - lives_label.get_width() - 10, 10)) #Ancho de la pantalla - ancho de la palabra - 10
-        
+        level_label = main_font.render(f"Nivel: {level}", 1, FONT_RED)
+        WIN.blit(lives_label, (10, 10)), WIN.blit(level_label, (WIDTH - lives_label.get_width() - 10, 10))  #Ancho de la pantalla - ancho de palabra - 10
+
         for enemy in enemies:
             enemy.draw(WIN)
 
-        player.draw(WIN) #Llamo al personaje
+        player.draw(WIN)
 
         pygame.display.update() #Updatea la pantalla
 
     while run:
-        clock.tick(FPS)
-        redraw_window() #llamo a la funcion para updatear la pantalla
+            clock.tick(FPS)
 
-        if len(enemies) == 0:   #Compruebo que cuando no hay mas enemigos subo el nivel
-            level += 1
-            wave_lenght += 5
-            for i in range(wave_lenght):
-                enemy = Enemy(random.randrange(50, WIDTH-100), random.randrange(-1000, -100), random.choice(["blue", "green", "yellow"]))
+            if len(enemies) == 0:
+                level += 1
+                wave_lenght += 0
+
+                enemy = Enemy(WIDTH, (HEIGHT / 2) - 95) #Spawn del enemigo
                 enemies.append(enemy)
-            
-        for event in pygame.event.get(): 
-            if event.type == pygame.QUIT: #Cuando el usuario preciona el boton de cerrar (X)
-                run = False #Cambio para que se cierre la ventana
-        
-        keys = pygame.key.get_pressed() #Declaro la variable que detecta las teclas presionadas
-        if keys[pygame.K_a] or keys[pygame.K_LEFT] and player.x - player_vel > 0: #Tecla a que va hacia la izquierda
-            player.x -= player_vel 
-        if keys[pygame.K_d] or keys[pygame.K_RIGHT] and player.x +  player_vel + player.get_width() < WIDTH: #Derecha (Despues del and compruebo que el character no pase los bordes de las ventanas)
-            player.x += player_vel
-        if keys[pygame.K_w] or keys[pygame.K_UP] and player.y - player_vel > 0: #Arriba
-            player.y -= player_vel
-        if keys[pygame.K_s] or keys[pygame.K_DOWN] and player.y +  player_vel + (player.get_height() + 30) < HEIGHT: #Abajo
-            player.y += player_vel
 
-        #Movimiento enemigos
-        for enemy in enemies:
-            enemy.move(enemy_vel)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False #Cambio el run a False, para que se cierre el juego
 
+            keys = pygame.key.get_pressed() #Declaro la variable que detecta las teclas presionadas
+            if keys[pygame.K_a] or keys[pygame.K_LEFT] and player.x - player_vel > 0: #Tecla a que va hacia la izquierda
+                player.x -= player_vel 
+            if keys[pygame.K_d] or keys[pygame.K_RIGHT] and player.x +  player_vel + player.get_width() < WIDTH: #Derecha (Despues del and compruebo que el character no pase los bordes de las ventanas)
+                player.x += player_vel
+            if keys[pygame.K_w] or keys[pygame.K_UP] and player.y - player_vel > 0: #Arriba
+                player.y -= player_vel
+            if keys[pygame.K_s] or keys[pygame.K_DOWN] and player.y +  player_vel + (player.get_height() + 30) < HEIGHT: #Abajo
+                player.y += player_vel
 
-main() #Llamo a la funcion Main par iniciar el juego
+            #Movimiento enemigos
+            for enemy in enemies:
+                enemy.move(enemy_vel)
+
+            redraw_window() #llamo a la funcion para updatear la pantalla
+main()
