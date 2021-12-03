64981261d92518e050643d417a0b4e4c1d308492
Gonzalo Boquete <gonzaboquete@hotmail.com>
2021-12-03 11:42:49 -0300

    Borre "pruebaMenu" ya que no es un archivo necesario

--- pruebaMenu.py
+++
@@ -1,6 +0,0 @@
-from game import Game   #importamos la clase Game del archivo game
-
-g = Game()  #creamos una variable para inicializar la clase Game
-
-while g.running:    #bucle que se repite mientras se ejecute el juego
-    g.curr_menu.display_menu()  #le pedimos a la variable de menu actual que muestre el menu
